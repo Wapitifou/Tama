5bfb22fe0f0b180162f9538b127c25219ed98ca6
Wapitifou <marianne.bonnard.nz@gmail.com>
2025-02-12 07:11:53 +0100

    Mini-jeu fonctionnel

--- index.js
+++ index.js
@@ -2,17 +2,64 @@ const LevelOfLife = document.getElementById("life");
 const LevelOfThirst = document.getElementById("thirst");
 const LevelOfHunger = document.getElementById("hunger");
 const LevelOfLove = document.getElementById("love");
+const water = document.getElementById("water");
+const pretzel = document.getElementById("pretzel");
+const hug = document.getElementById("hug");
 let thirst = 0;
 let hunger = 0;
-let love = "10❤️";
+let love = 10;
 let life = ["🐹", "🐹", "🐹", "🐹", "🐹"];
 
 LevelOfLife.innerText = `Life: ${life.join(" ")}`;
 LevelOfThirst.innerText = `Thirst: ${thirst}`;
 LevelOfHunger.innerText = `Hunger: ${hunger}`;
-LevelOfLove.innerText = `Love: ${love}`;
+LevelOfLove.innerText = `Love: ${love}❤️`;
 
-function lifespan() {}
-function feed() {}
-function drink() {}
-function pet() {}
+function majAffichage() {
+  LevelOfLife.innerText = `Life: ${life.join(" ")}`;
+  LevelOfThirst.innerText = `Thirst: ${thirst}`;
+  LevelOfHunger.innerText = `Hunger: ${hunger}`;
+  LevelOfLove.innerText = `Love: ${love}❤️`;
+}
+
+function needs() {
+  setInterval(() => {
+    hunger++;
+    thirst++;
+    love--;
+    majAffichage();
+  }, 3000);
+}
+
+function lifespan() {
+  setInterval(() => {
+    if (hunger >= 5 || thirst >= 5 || love <= 5) {
+      life.pop();
+    }
+    majAffichage();
+  }, 3000);
+}
+function drink() {
+  water.addEventListener("click", (e) => {
+    thirst--;
+    majAffichage();
+  });
+}
+function feed() {
+  pretzel.addEventListener("click", (e) => {
+    hunger--;
+    majAffichage();
+  });
+}
+function pet() {
+  hug.addEventListener("click", (e) => {
+    love++;
+    majAffichage();
+  });
+}
+
+drink();
+needs();
+feed();
+pet();
+lifespan();
